0b4222286425f19e5ef95f320768acb4e71ac79b
Skye Book <skye.book@gmail.com>
2011-11-17 13:05:43 -0500

    Created EmptyResponseTest

---
+++ Test/net/skyebook/betaville/json/EmptyResponseTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package net.skyebook.betaville.json;
+
+import java.util.List;
+
+import edu.poly.bxmc.betaville.model.Design;
+
+/**
+ * @author Skye Book
+ *
+ */
+public class EmptyResponseTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JSONClientManager jcm = new JSONClientManager();
+		long startTime = System.currentTimeMillis();
+		List<Design> designs = jcm.findAllDesignsByCity(20);
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println("Took " + (endTime - startTime) + " ms");
+	}
+
+}
